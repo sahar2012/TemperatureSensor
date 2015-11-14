bdf15f2a6e6af206be00f9e95fb3a0e622d87795
sahar2012 <sahar2012@gmail.com>
2015-11-14 14:38:58 +0530

    SerializeTemperature updated

--- src/com/iiitd/ap/lab10/SerializeTemperature.java
+++ src/com/iiitd/ap/lab10/SerializeTemperature.java
@@ -1,18 +1,37 @@
-//@author
 package com.iiitd.ap.lab10;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 
 public class SerializeTemperature extends Observer
 {
 
 	public SerializeTemperature(TemperatureSensor temperatureSensor)
 	{
-		
+		this.temperatureSensor=temperatureSensor;
+		this.temperatureSensor.register(this);
 	}
 	
 	@Override
-	public void update() {
-		// TODO Auto-generated method stub
-		
+	public void update() 
+	{
+		try {
+			FileOutputStream file = new FileOutputStream("src"+File.separator+"TemperatureLogsFile.txt");
+			ObjectOutputStream out = new ObjectOutputStream(file);
+			out.writeObject(this.temperatureSensor.getLogs());
+			out.close();
+			file.close();
+		} 
+		catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 }
