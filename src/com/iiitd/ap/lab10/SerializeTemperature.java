677c2ed0b8c114e872fa92b1b5d0559bf84656e2
sahar2012 <sahar2012@gmail.com>
2015-11-14 13:30:54 +0530

    changed SerializeTemperature

--- src/com/iiitd/ap/lab10/SerializeTemperature.java
+++ src/com/iiitd/ap/lab10/SerializeTemperature.java
@@ -1,6 +1,17 @@
 package com.iiitd.ap.lab10;
 
-public class SerializeTemperature 
+public class SerializeTemperature extends Observer
 {
 
+	public SerializeTemperature(TemperatureSensor temperatureSensor)
+	{
+		
+	}
+	
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
